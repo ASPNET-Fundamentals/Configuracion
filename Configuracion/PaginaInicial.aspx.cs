04e961915629a03aa8e7612acafce1c602aed71e
Juan José Morra <juan_morra@edsa.com.ar>
2020-10-02 16:54:23 -0300

    Commit inicial

---
+++ Configuracion/PaginaInicial.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Configuracion
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string ejemploKey = ConfigurationManager.AppSettings["EjemploKey"];
+            string ejemploKeyExternal = ConfigurationManager.AppSettings["EjemploKeyExternal"];
+            string stringConnection = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+
+            Response.Write(string.Concat("Value Key EjemploKey: ", ejemploKey));
+            Response.Write("<BR/>");
+            Response.Write(string.Concat("Valor Key EjemploKeyExternal: ", ejemploKeyExternal));
+            Response.Write("<BR/>");
+            Response.Write(string.Concat("Value Key stringConnection: ", stringConnection));            
+        }
+    }
+}
